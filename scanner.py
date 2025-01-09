0c53c5adc69634947476a7bd72dee1760f6d993f
JeanMarie <kiizajeanmarie@gmail.com>
2025-01-09 18:15:52 +0000

    initial commit

---
+++ scanner.py
@@ -0,0 +1,44 @@
+#!/bin/python3:
+
+#JeanMarie Kiiza 
+#1/5/2025
+#Port scanner Project 
+import sys
+import socket
+from datetime import datetime 
+#Define our target
+
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #translate host name into ipv4
+else:
+	print("invalid amount of arguments.")
+	print("syntax: python3 scanner.py <ip>")
+	
+# makes it look good and readable 
+print("-" * 50)
+print("Scanning target: "+target)
+print("Time started: "+str(datetime.now()))
+print("-" * 50)
+
+# Loop for the port scanner, you can chnage the range into anything
+try:
+	for port in range(58, 100):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port))
+		if result == 0:
+			print(f"port {port} is open")
+		s.close()
+# if we want our port scanner to stop, or we encountered errors. 
+		
+except KeyboardInterrupt:
+	print("\nExiting program.")
+	sys.exit()
+	
+except socket.gaierror:
+	print("Host name could not be resolved.")
+	sys.exit()
+	
+except socket.error:
+	print("could not connect to server.")
+	sys.exit()
